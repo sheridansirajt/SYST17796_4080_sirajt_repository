54b337ab5fbef9ebf05dc0afe1d5b5ff8b882aa7
Taha Siraj <sirajt@sheridancollege.ca>
2020-01-22 18:05:56 -0500

    Test

---
+++ SoftwareDesignandDevelopment/src/inClass/ListArray.java
@@ -0,0 +1,11 @@
+package inClass;
+
+import java.util.Arrays;
+
+public class ListArray {
+
+	public static void main(String[] args) {
+		String[] arr = {"Movie1","Movie2","Movie3"};
+		System.out.print(Arrays.toString(arr));
+	}
+}
