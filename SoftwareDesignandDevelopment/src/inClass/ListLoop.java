512f581463e4f236207003b26faa469a6b04ded7
sheridansirajt <59941442+sheridansirajt@users.noreply.github.com>
2020-01-22 18:41:06 -0500

    Update ListLoop.java

--- SoftwareDesignandDevelopment/src/inClass/ListLoop.java
+++ SoftwareDesignandDevelopment/src/inClass/ListLoop.java
@@ -5,10 +5,6 @@ import java.util.*;
 public class ListLoop {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World");
-=======
 		System.out.println("Hello Earth");
->>>>>>> branch 'master' of https://github.com/sheridansirajt/SYST17796_4080_sirajt_repository.git
 	}
 }
