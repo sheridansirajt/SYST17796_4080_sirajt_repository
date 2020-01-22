7f647677d21a76e0e1f1eabd4b1ea8d756efae0f
Taha Siraj <sirajt@sheridancollege.ca>
2020-01-22 18:28:03 -0500

    Update ListLoop.java

--- SoftwareDesignandDevelopment/src/inClass/ListLoop.java
+++ SoftwareDesignandDevelopment/src/inClass/ListLoop.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class ListLoop {
 
 	public static void main(String[] args) {
+		System.out.println();
+<<<<<<< HEAD
 
+=======
+>>>>>>> bb9d7bc4e6159dbee725b6aaf05072897e0cf3fb
 	}
 }
