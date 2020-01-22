953064c54a19133232c19fbfd1d4498001f988bc
sheridansirajt <59941442+sheridansirajt@users.noreply.github.com>
2020-01-22 18:34:07 -0500

    Test

--- SoftwareDesignandDevelopment/src/inClass/ListLoop.java
+++ SoftwareDesignandDevelopment/src/inClass/ListLoop.java
@@ -5,13 +5,6 @@ import java.util.*;
 public class ListLoop {
 
 	public static void main(String[] args) {
-		System.out.println();
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bb9d7bc4e6159dbee725b6aaf05072897e0cf3fb
-=======
->>>>>>> bb9d7bc4e6159dbee725b6aaf05072897e0cf3fb
+		System.out.println("Hello Earth");
 	}
 }
