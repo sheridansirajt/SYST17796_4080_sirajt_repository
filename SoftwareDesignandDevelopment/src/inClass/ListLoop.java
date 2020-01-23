357535f24a46e538f042a4fbdcb0fcad55cc94c2
Taha Siraj <sirajt@sheridancollege.ca>
2020-01-22 19:37:59 -0500

    test

--- SoftwareDesignandDevelopment/src/inClass/ListLoop.java
+++ SoftwareDesignandDevelopment/src/inClass/ListLoop.java
@@ -7,6 +7,5 @@ public class ListLoop {
 	public static void main(String[] args) {
 		System.out.println("Hello Earth");
 		System.out.println("Hello World");
-		System.out.println("Hello Sun");
 	}
 }
